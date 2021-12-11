92638b0e92fc8050217a4ba056832f52dff4c45d
rodiemertomi <rodiemertominez@gmail.com>
2021-12-11 10:02:36 +0800

    changes the input box to select type in AdminDeposit

--- src/components/admin_controls/AdminDeposit.jsx
+++ src/components/admin_controls/AdminDeposit.jsx
@@ -1,17 +1,20 @@
-import React, { useState } from 'react'
+import React, { useState, useEffect } from 'react'
+import { formatPrice } from '../helpers'
 
 const AdminDeposit = () => {
 	const [userName1, setUserName1] = useState('')
 	const [amount, setAmount] = useState(0)
+	const [users, setUsers] = useState([])
+	// const [user, setUser] = useState('')
+	const userObj = JSON.parse(localStorage.getItem(`${userName1}`))
 
 	const sendTo = (username1, peso) => {
 		if (peso < 0 || peso === 0) {
 			alert('Amount cannot be less than or equal to zero.')
 		} else {
-			const userObj = JSON.parse(localStorage.getItem(`${userName1}`))
-			userObj.balance += parseInt(peso)
+			userObj.balance += parseInt(peso * 100)
 			localStorage.setItem(`${username1}`, JSON.stringify(userObj))
-			alert(`${amount} deposited to ${userName1}`)
+			alert(`${formatPrice(amount)} deposited to ${userName1}`)
 
 			setUserName1('')
 			setAmount(0)
@@ -20,7 +23,6 @@ const AdminDeposit = () => {
 
 	const checkUser = event => {
 		event.preventDefault()
-		const userObj = JSON.parse(localStorage.getItem(`${userName1}`))
 		if (userName1 !== '' && amount !== null && amount !== 0) {
 			if (userObj !== null) {
 				sendTo(userName1, amount)
@@ -32,19 +34,34 @@ const AdminDeposit = () => {
 		}
 	}
 
+	useEffect(() => {
+		let updatedUsers = []
+		getUserKeys(updatedUsers)
+		setUsers(updatedUsers)
+	}, [])
+
+	const getUserKeys = updatedUsers => {
+		for (let i = 0; i < localStorage.length; ++i) {
+			updatedUsers.push(localStorage.key(i))
+		}
+	}
 	return (
 		<div>
 			<form className='deposit-form'>
 				<h1>Deposit Form</h1>
 				<label className='labels'>To:</label>
-				<input
-					type='text'
-					className='inputs'
-					placeholder='Enter Username'
+				<select
+					value={userName1}
 					onChange={e => {
 						setUserName1(e.target.value)
 					}}
-				/>
+				>
+					{users.map(item => (
+						<option key={Date.now} value={`${item}`}>
+							{`${item}`}
+						</option>
+					))}
+				</select>
 				<label className='labels'>Amount:</label>
 				<input
 					type='number'
